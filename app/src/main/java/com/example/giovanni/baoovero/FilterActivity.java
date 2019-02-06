bb93ff87e34fc2c483b40a304d110e31ffa95e94
ruoccogiovanni <gionni0797@gmail.com>
2019-02-06 20:11:44 +0100

    Aggiunto inizio dell'inizio del filtro

--- app/src/main/java/com/example/giovanni/baoovero/FilterActivity.java
+++ app/src/main/java/com/example/giovanni/baoovero/FilterActivity.java
@@ -17,18 +17,15 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class FilterActivity extends AppCompatActivity {
 
     List<Dog> listacani;
 
 
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle t;
-    private NavigationView nv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_filter);
 
         listacani = new ArrayList<>();
         listacani.add(new Dog("Oliver","West Highland White Terrier","Oliviiiiiiiero","Maschio","Napoli", "4","3463919107", "gionni0797@gmail.com", R.drawable.image_olivi));
@@ -43,56 +40,19 @@ public class MainActivity extends AppCompatActivity {
         listacani.add(new Dog("Oliver","West Highland White Terrier","Oliviiiiiiiero","Maschio","Napoli", "4","3463919107", "gionni0797@gmail.com", R.drawable.image_westie));
         listacani.add(new Dog("Giselle","Briard","Patàààà","Femmina", "Caserta", "8", "3932791138", "ivanorefix3@yahoo.com", R.drawable.image_gisy));
 
-        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
+        
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_filter);
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,listacani);
         myrv.setLayoutManager(new GridLayoutManager(this,1));
         myrv.setAdapter(myAdapter);
-        
-        final Intent intentprofile = new Intent(this, FilterActivity.class);
-        final Intent intentlogin = new Intent(this, LoginActivity.class);
-        final Intent intentadd = new Intent(this, Add_Activity.class);
 
-
-        dl = (DrawerLayout)findViewById(R.id.dl);
-        t = new ActionBarDrawerToggle(this, dl, R.string.open, R.string.close);
-        t.setDrawerIndicatorEnabled(true);
-
-        dl.addDrawerListener(t);
-        t.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        nv = (NavigationView)findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch(id)
-                {
-                    case R.id.profile:
-                        Toast.makeText(MainActivity.this, "Profile",Toast.LENGTH_SHORT).show();
-                        startActivity(intentprofile);
-
-                        break;
-                    case R.id.settings:
-                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
-                        startActivity(intentadd);
-                        break;
-                        case R.id.about:
-                        Toast.makeText(MainActivity.this, "addupattt't",Toast.LENGTH_SHORT).show();
-                        startActivity(intentlogin);
-                        break;
-
-                }
-
-
-                return true;
 
             }
-        });
 
 
-    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
