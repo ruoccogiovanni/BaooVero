20d47447468b2c15c26eeda439fec34c1dea09d7
ruoccogiovanni <gionni0797@gmail.com>
2019-02-09 17:25:28 +0100

    Acconciate cosine

--- app/src/main/java/com/example/giovanni/baoovero/ProfileActivity.java
+++ app/src/main/java/com/example/giovanni/baoovero/ProfileActivity.java
@@ -3,6 +3,9 @@ package com.example.giovanni.baoovero;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -52,5 +55,22 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflauto = getMenuInflater();
+        inflauto.inflate(R.menu.profilefav_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if (item.getItemId()==R.id.preferiti)
+        {
+            Toast.makeText(getApplicationContext(),"Fevorit",Toast.LENGTH_SHORT).show();
+            //startActivity(new Intent(ProfileActivity.this,FilterActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
