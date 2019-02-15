75597febda5d17ac78b14a48306f83f9f7ea8b29
LucaCampanile <luca.campanile@studenti.unicampania.it>
2019-02-15 19:31:29 +0100

    Si prende i preferiti

--- app/src/main/java/com/example/giovanni/baoovero/FavouriteActivity.java
+++ app/src/main/java/com/example/giovanni/baoovero/FavouriteActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,28 +23,48 @@ public class FavouriteActivity extends AppCompatActivity {
     private List<Dog> listacani;
     private RecyclerViewAdapter myAdapter;
     private DatabaseReference myRef;
-
+    private DatabaseReference myRef2;
+    private FirebaseAuth auth;
+    private String utente;
+    private List<String> uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
-
+        auth = FirebaseAuth.getInstance();
+        utente = auth.getCurrentUser().getUid();
         listacani=new ArrayList<>();
-        myRef= FirebaseDatabase.getInstance().getReference();
+        uid = new ArrayList();
+
+        myRef= FirebaseDatabase.getInstance().getReference("Cani");
+        myRef2 = FirebaseDatabase.getInstance().getReference("Utenti");
         myrv = (RecyclerView) findViewById(R.id.favourite_recyclerview);
         myrv.setLayoutManager(new GridLayoutManager(this, 1));
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-              for(DataSnapshot posSnapshot: dataSnapshot.getChildren())  {
-                  String uid = posSnapshot.child("Utenti").child("Preferiti").getValue().toString();
-                  Dog cane = posSnapshot.child("Cani").getValue(Dog.class);
-                  if(cane.getUid()==uid) listacani.add(cane);
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot posSnapshot: dataSnapshot.getChildren())  {
+                            Dog cane = posSnapshot.getValue(Dog.class);
+                            for (String s: uid) {
+                                System.out.println(cane.getUid() + "ooooooooooooooooooooo " + s);
+                                if (cane.getUid().equals(s)) {
+                                    listacani.add(cane);
+                                }
+                            }
+                            myAdapter=new RecyclerViewAdapter(FavouriteActivity.this,listacani);
+                            myrv.setAdapter(myAdapter);
+                        }
+                    }
 
-              }
-                myAdapter=new RecyclerViewAdapter(FavouriteActivity.this,listacani);
-                myrv.setAdapter(myAdapter);
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+                uid.add(dataSnapshot.child(utente).child("Preferiti").getValue(String.class));
 
             }
 
