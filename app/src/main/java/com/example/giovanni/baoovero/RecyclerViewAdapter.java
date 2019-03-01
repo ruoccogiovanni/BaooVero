cbd0cc91e363a0a65ff5a05a883c9389b1b63a79
ivanorefix3@yahoo.com <Gianni023!>
2019-03-01 16:59:19 +0100

    Cambiato finalmente la gif caricamento

--- app/src/main/java/com/example/giovanni/baoovero/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/giovanni/baoovero/RecyclerViewAdapter.java
@@ -1,10 +1,7 @@
 package com.example.giovanni.baoovero;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
 import java.util.List;
 
