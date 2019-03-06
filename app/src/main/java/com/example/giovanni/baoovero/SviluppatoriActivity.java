1663a91f35635b5335140c618f1587aa9f795639
ivanorefix3@yahoo.com <Gianni023!>
2019-03-06 12:35:21 +0100

    I nomi degli sviluppatori ci volevano òh

--- app/src/main/java/com/example/giovanni/baoovero/SviluppatoriActivity.java
+++ app/src/main/java/com/example/giovanni/baoovero/SviluppatoriActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.luolc.emojirain.EmojiRainLayout;
 
 public class SviluppatoriActivity extends AppCompatActivity {
